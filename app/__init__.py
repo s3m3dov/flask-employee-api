59c85b92c1adf765cb8fe025640784732815a8d5
Hikmat Samadov <s3m3dov@hotmail.com>
2023-05-04 15:53:02 +0400

    refactor: update settings and doc urls

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,27 @@
 """Team Manager server application"""
+from typing import Optional, Dict, Any
 
 from flask import Flask
 
 from app import extensions, views
-from app.config import DefaultConfig
+from app.config import ProductionConfig, DebugConfig
 
 
-def create_app():
+def create_app(test_config: Optional[Dict[str, Any]] = None):
     """Create application"""
-    app = Flask('Team manager')
+    app = Flask("Team manager")
 
-    app.config.from_object(DefaultConfig)
+    # Loading config...
+    # Load defaults
+    _config = app.config
+    flask_debug: bool = _config.get("DEBUG", False)
+    if flask_debug:
+        _config.from_object(DebugConfig)
+    elif test_config is None:
+        # only load production defaults if no special test config is given
+        _config.from_object(ProductionConfig)
     # Override config with optional settings file
-    app.config.from_envvar('FLASK_SETTINGS_FILE', silent=True)
+    app.config.from_envvar("FLASK_SETTINGS_FILE", silent=True)
 
     api = extensions.create_api(app)
     views.register_blueprints(api)
---
+++ app/config/core.py
@@ -0,0 +1,5 @@
+class DefaultConfig:
+    API_TITLE = "Team Manager API"
+    API_VERSION = 0.1
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
